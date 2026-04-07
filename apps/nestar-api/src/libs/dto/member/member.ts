810661088a3ea87bbda418965607630f486bb1a2
UktamSam <holmatov00@gmail.com>
2026-04-07 20:58:13 +0900

    feat: develop signup business logic

---
+++ apps/nestar-api/src/libs/dto/member/member.ts
@@ -0,0 +1,82 @@
+import { Field, Int, ObjectType } from "@nestjs/graphql";
+import { ObjectId } from "mongoose";
+import { MemberAuthType, MemberStatus, MemberType } from "../../enums/member.enum";
+
+
+
+@ObjectType()           //DTO 
+export class Member {
+    @Field(() => String)
+    _id: ObjectId;
+
+    @Field(() => MemberType)
+    memberType: MemberType;
+
+    @Field(() => MemberStatus)
+    memberStatus: MemberStatus;
+
+    @Field(() => MemberAuthType)
+    memberAuthType: MemberAuthType;
+
+    @Field(() => String)
+    memberPhone: string;
+
+    @Field(() => String)
+    memberNick: string;
+
+    memberPassword?: string; // Field'ga biriktirilmagan, chunki bu ma'lumotni tashqariga bermaymiz
+
+    @Field(() => String, { nullable: true }) //bo'lmasligi mumkin
+    memberFullName?: string;
+
+    @Field(() => String)
+    memberImage: string;
+
+    @Field(() => String, { nullable: true }) // bo'sh bo'lishi mumkin
+    memberAddress: string;
+
+    @Field(() => String, { nullable: true }) // bo'sh bo'lishi mumkin
+    memberDesc?: string;
+
+    @Field(() => Int)
+    memberProperties: number;
+
+    @Field(() => Int)
+    memberArticles: number;
+
+    @Field(() => Int)
+    memberFollowers: number;
+
+    @Field(() => Int)
+    memberFollowings: number;
+
+    @Field(() => Int)
+    memberPoints: number;
+
+    @Field(() => Int)
+    memberLikes: number;
+    
+    @Field(() => Int)
+    memberViews: number;
+
+    @Field(() => Int)
+    memberComments: number;
+
+    @Field(() => Int)
+    memberRanks: number;
+
+    @Field(() => Int)
+    memberWarnings: number;
+
+    @Field(() => Int)
+    memberBlocks: number;
+
+    @Field(() => Date, { nullable: true })
+    deletedAt?: Date;
+
+    @Field(() => Date)
+    createdAt: Date;
+
+    @Field(() => Date)
+    updatedAt: Date;    
+}    
