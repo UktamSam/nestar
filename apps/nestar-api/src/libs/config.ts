b26f09c79d554573fbe097526eec69a3e5e3869c
UktamSam <holmatov00@gmail.com>
2026-04-14 14:42:01 +0900

    feat: develop getAgents GraphQL API

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -2,4 +2,6 @@ import { ObjectId } from 'bson';
 
 export const shapeIntoMongoObjectId = (target: any) => {
     return typeof target === 'string' ? new ObjectId(target) : target;
-}
+} 
+
+export const availableAgentSorts = ['createdAt', 'updatedAt', 'memberRank', 'memberPoints', 'memberProperties', 'memberArticles', 'memberFollowers', 'memberFollowings', 'memberLikes', 'memberViews'];
