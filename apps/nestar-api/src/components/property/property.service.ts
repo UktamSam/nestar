c97795e7c501e02dc220d589ed027837d55193f0
UktamSam <holmatov00@gmail.com>
2026-04-18 17:51:25 +0900

    feat: develop getAgentProperties business logic

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -4,7 +4,7 @@ import { AuthService } from '../auth/auth.service';
 import { ViewService } from '../view/view.service';
 import { Model } from 'mongoose';
 import { Properties, Property } from '../../libs/dto/property/property';
-import { PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
+import { AgentPropertiesInquiry, PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
 import { Direction, Message } from '../../libs/enums/common.enum';
 import { MemberService } from '../member/member.service';
 import { ObjectId } from 'mongoose';
@@ -169,5 +169,39 @@ public async getProperties(memberId: ObjectId, input: PropertiesInquiry): Promis
                 return { [ele]: true };
             });
             }
+    }
+// -------------------------------------------------------------------------------------------------
+
+    public async getAgentProperties(memberId: ObjectId, input: AgentPropertiesInquiry): Promise<Properties> {
+        const { propertyStatus } = input.search;
+        if ( propertyStatus === PropertyStatus.DELETE) throw new BadRequestException(Message.NOT_ALLOWED_REQUEST);
+
+        const match: T = {
+            memberId: memberId, 
+            propertyStatus: propertyStatus ?? {$ne: PropertyStatus.DELETE},
+        };
+        const sort: T = {[input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC};
+
+        const result = await this.propertyModel
+            .aggregate([
+                {$match: match},
+                {$sort: sort},
+                {
+                    $facet: {
+                        properties: [
+                            {$skip: (input.page - 1) * input.limit}, 
+                            {$limit: input.limit},
+                            lookUpMember,
+                            { $unwind: '$memberData' },
+    
+                        ],
+                        metaCounter: [{$count: 'total'}],
+                    },
+                },
+            ])
+            .exec();
+
+        if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+        return result[0];
     }
 }
