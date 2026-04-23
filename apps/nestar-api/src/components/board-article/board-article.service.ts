976b4466c1e0eca7208029bd6193aba13f409e9a
UktamSam <holmatov00@gmail.com>
2026-04-23 15:23:38 +0900

    feat: develop likeTargetBoardArticle GraphQL API

--- apps/nestar-api/src/components/board-article/board-article.service.ts
+++ apps/nestar-api/src/components/board-article/board-article.service.ts
@@ -12,12 +12,16 @@ import { InternalServerErrorException } from '@nestjs/common';
 import { ViewGroup } from '../../libs/enums/view.enum';
 import { BoardArticleUpdate } from '../../libs/dto/board-article/board-article.update';
 import { lookUpMember, shapeIntoMongoObjectId } from '../../libs/config';
+import { LikeService } from '../like/like.service';
+import { LikeInput } from '../../libs/dto/like/like.input';
+import { LikeGroup } from '../../libs/enums/like.enum';
 
 @Injectable()
 export class BoardArticleService {
     constructor(@InjectModel("BoardArticle") private readonly boardArticleModel: Model<BoardArticle>,
     private readonly viewService: ViewService,
     private readonly memberService: MemberService,
+    private readonly likeService: LikeService,
 ) {} 
 // -------------------------------------------------------------------------------------------------
 
@@ -114,7 +118,25 @@ export class BoardArticleService {
         if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
         return result[0];
     }
+// -------------------------------------------------------------------------------------------------
 
+    public async likeTargetBoardArticle(memberId: ObjectId, likeRefId: ObjectId): Promise<BoardArticle> {
+        const target: BoardArticle = await this.boardArticleModel.findOne({ _id: likeRefId, articleStatus: BoardArticleStatus.ACTIVE }).exec(); // biz bosmoqchi bolgan boardArticleni tekshirish`
+        if (!target) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+        const input: LikeInput = {
+            memberId: memberId,
+            likeRefId: likeRefId,
+            likeGroup: LikeGroup.ARTICLE,
+        };
+
+        //Like TOGGLE via Like modules
+        const modifier: number = await this.likeService.toggleLike(input);
+        const result = await this.boardArticleStatsEditor({_id: likeRefId, targetKey: 'articleLikes', modifier: modifier});
+
+        if (!result) throw new InternalServerErrorException(Message.SOMETHING_WENT_WRONG);
+        return result;
+    }
 //===============================================================================================================
 
     public async getAllBoardArticlesByAdmin(input: AllBoardArticlesInquiry): Promise<BoardArticles> {
