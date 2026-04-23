13ca5d8d7c113d93be9545991e855a5c31f3535f
UktamSam <holmatov00@gmail.com>
2026-04-23 15:09:44 +0900

    feat: develop likeTargetProperty GraphQL API

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -14,6 +14,9 @@ import { ViewGroup } from '../../libs/enums/view.enum';
 import { PropertyUpdate } from '../../libs/dto/property/property.update';
 import moment = require('moment');
 import { lookUpMember, shapeIntoMongoObjectId } from '../../libs/config';
+import { LikeService } from '../like/like.service';
+import { LikeInput } from '../../libs/dto/like/like.input';
+import { LikeGroup } from '../../libs/enums/like.enum';
 
 
 @Injectable()
@@ -22,6 +25,7 @@ export class PropertyService {
                     private authservice: AuthService,
                     private viewService: ViewService,
                     private memberService: MemberService,
+                    private likeService: LikeService,
                 ) {}
 // -------------------------------------------------------------------------------------------------
 public async createProperty(input: PropertyInput): Promise<Property>{
@@ -194,7 +198,25 @@ public async getProperties(memberId: ObjectId, input: PropertiesInquiry): Promis
         if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
         return result[0];
     }
+// -------------------------------------------------------------------------------------------------
 
+    public async likeTargetProperty(memberId: ObjectId, likeRefId: ObjectId): Promise<Property> {
+        const target: Property = await this.propertyModel.findOne({ _id: likeRefId, propertyStatus: PropertyStatus.ACTIVE }).exec(); // biz bosmoqchi bolgan propertyni tekshirish`
+        if (!target) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+        const input: LikeInput = {
+            memberId: memberId,
+            likeRefId: likeRefId,
+            likeGroup: LikeGroup.PROPERTY,
+        };
+
+        //Like TOGGLE via Like modules
+        const modifier: number = await this.likeService.toggleLike(input);
+        const result = await this.propertyStatsEditor({_id: likeRefId, targetKey: 'propertyLikes', modifier: modifier});
+
+        if (!result) throw new InternalServerErrorException(Message.SOMETHING_WENT_WRONG);
+        return result;
+    }
 // -------------------------------------------------------------------------------------------------
 
     public async getAllPropertiesByAdmin(input: AllPropertiesInquiry): Promise<Properties> {
