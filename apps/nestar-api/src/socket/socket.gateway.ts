5349e019366015cc2732829244710ee6b53e8f80
UktamSam <holmatov00@gmail.com>
2026-05-28 20:52:22 +0900

    fix: modify WebSocket Chatting Server final stage

--- apps/nestar-api/src/socket/socket.gateway.ts
+++ apps/nestar-api/src/socket/socket.gateway.ts
@@ -1,21 +1,31 @@
 import { Logger } from '@nestjs/common';
 import { OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server } from 'ws';
+import { AuthService } from '../components/auth/auth.service';
+import { Member } from '../libs/dto/member/member';
+import url from 'url';
 import * as WebSocket from 'ws';
 
 interface MessagePayload {
 	event: string;
 	text: string;
+	memberData: Member | null;
 }
 interface InfoPayload {
 	event: string;
 	totalClients: number;
+	memebrData: Member | null;
+	action: string;
 }
 
 @WebSocketGateway({ transports: ['websocket'], secure: false })
 export class SocketGateway implements OnGatewayInit{
   private logger: Logger = new Logger('SocketEvents')
   private summaryClient: number = 0;
+	private clientsAuthMap = new Map<WebSocket, Member | null>();
+	private messageList: MessagePayload[] = [];
+
+	constructor(private authService: AuthService) {}
 
 	@WebSocketServer()
 	server: Server;
@@ -24,25 +34,55 @@ export class SocketGateway implements OnGatewayInit{
 		this.logger.verbose(`WebSocket Server Initialized total: [${this.summaryClient}]`);
   }
 
-  handleConnection(client: WebSocket, ...arg: any[]) {
+	private async retrieveAuth(req: any): Promise<Member | null> {
+		try {
+			const parsedUrl = url.parse(req.url, true);
+			const { token } = parsedUrl.query as { token: string };
+			console.log('token', token);
+			if (!token) {
+				throw new Error('Token not found');
+			}
+			const authMember = await this.authService.verifyToken(token);
+			return authMember;
+		} catch (error) {
+			return null;
+		}
+	}
+	public async handleConnection(client: WebSocket, req: any) {
+		const authMember = await this.retrieveAuth(req);
     this.summaryClient++;
-		this.logger.verbose(`Connection & total: [${this.summaryClient}]`);
+
+		console.log('authMember', authMember);
+		this.clientsAuthMap.set(client, authMember);
+
+		const clientNick = authMember?.memberNick ?? 'Guset';
+		this.logger.verbose(`${clientNick} connected & total: [${this.summaryClient}]`);
 
 		const infoMessage: InfoPayload = {
 			event: 'info',
 			totalClients: this.summaryClient,
+			memebrData: authMember,
+			action: 'joined',
 		};
 
 		this.emitMesage(infoMessage);
+		client.send(JSON.stringify({ event: 'getMessages', list: this.messageList }));
   }
 
-  handleDisconnect(client: WebSocket) {
+	public handleDisconnect(client: WebSocket) {
+		const authMember = this.clientsAuthMap.get(client);
     this.summaryClient--;
+		this.clientsAuthMap.delete(client);
+		const clientNick = authMember?.memberNick ?? 'Guset';
+
+		console.log('client:', client);
 		this.logger.verbose(`Disconnection & total: [${this.summaryClient}]`);
 
 		const infoMessage: InfoPayload = {
 			event: 'info',
 			totalClients: this.summaryClient,
+			memebrData: authMember ?? null,
+			action: 'left',
 		};
 
 		this.broadcastMessage(client, infoMessage);
@@ -50,12 +90,20 @@ export class SocketGateway implements OnGatewayInit{
 
   @SubscribeMessage('message')
 	public async handleMessage(client: WebSocket, payload: string): Promise<void> {
+		const authMember = this.clientsAuthMap.get(client);
 		const newMessage: MessagePayload = {
 			event: 'message',
 			text: payload,
+			memberData: authMember ?? null,
 		};
 
-		this.logger.verbose(`NewMessage: ${payload}`);
+		const clientNick = authMember?.memberNick ?? 'Guset';
+		this.logger.verbose(`NewMessage: ${clientNick}: ${payload}`);
+
+		this.messageList.push(newMessage);
+		if (this.messageList.length > 5) {
+			this.messageList.splice(0, this.messageList.length - 5);
+		}
 		this.emitMesage(newMessage);
 	}
 
@@ -75,3 +123,9 @@ export class SocketGateway implements OnGatewayInit{
 		});
   }
 }
+
+/**
+ * 1. Client (Faqat o'sha clientga)
+ * 2. Broadcast (o'sha clientdan tashqari Barcha clientlarga)
+ * 3. Emit (Barcha clientlarga)
+ */
